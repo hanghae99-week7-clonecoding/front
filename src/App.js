9829df6ab0c90e2dbc9e23cf467583a4ff53d625
JJOOYYONG <https://github.com/JJOOYYONG>
2022-08-19 21:02:43 +0900

    폴더파일 재공유

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
-import logo from "./logo.svg";
-
+import Route from "./shared/Router";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Route />;
 }
 
 export default App;

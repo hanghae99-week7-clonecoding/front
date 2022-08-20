7a1166d2a629c3991bb8d4dc3e886e32fe833de5
JJOOYYONG <https://github.com/JJOOYYONG>
2022-08-20 23:20:36 +0900

    상세보기 댓글 및 회원가입 화면

--- src/components/features/Comment.jsx
+++ src/components/features/Comment.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../css_modules/Comment.module.css";
+//btn
+import Btn from "../elements/Btn";
 
 const Comment = () => {
-  return <div></div>;
+  //Hook
+  const [checkBtn, setCheckBtn] = useState(false);
+  //이벤트 함수
+  const showBtn = () => {
+    return (
+      <div className={styles.btnWrap}>
+        <Btn>취소</Btn>
+        <Btn backgroundColor="ligth-gray" color="gray" height="40px">
+          등록
+        </Btn>
+      </div>
+    );
+  };
+  return (
+    <div className={styles.commentWrap}>
+      <div className={styles.writeContent}>
+        <div className={styles.commentWrite}>
+          <div className={styles.profile}>
+            {/* 이미지는 해당영역의 백그라운드로 들어갈 예정 */}
+            <div></div>
+          </div>
+          <div className={styles.comment}>
+            <input
+              type="text"
+              name="comment"
+              placeholder="댓글추가..."
+              onClick={() => setCheckBtn(true)}
+            />
+          </div>
+        </div>
+        {checkBtn ? showBtn() : null}
+      </div>
+      <div className={styles.listContent}></div>
+    </div>
+  );
 };
 
 export default Comment;
